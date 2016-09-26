f9ef4104bc44a33733dd9ac977c4d63db389358c
Brian Black <black@gatech.edu>
2016-09-25 21:12:11 -0400

    sphere

--- app.js
+++ app.js
@@ -1,4 +1,73 @@
 $(() => {
+    var stacks = 6;
+    var slices = 12;
+    var sphere = {
+        model: {
+            frames: [{
+                verts: _.chain(stacks)
+                    .times(_.identity)
+                    .inject((memo, stack) => {
+                        _.times(slices, (slice) => {
+                            var v = vec3.fromValues(0, 1, 0);
+                            vec3.rotateZ(v, v, vec3.create(), Math.PI*((stack+1)/(stacks+1)));
+                            vec3.rotateY(v, v, vec3.create(), 2*Math.PI*slice/slices);
+                            memo.push(v);
+                        });
+                        return memo;
+                    }, [])
+                    .push(vec3.fromValues(0, 1, 0))
+                    .push(vec3.fromValues(0, -1, 0))
+                    .value()
+            }],
+            triangles: _.chain(stacks - 1)
+                .times(_.identity)
+                .inject((memo, stack) => {
+                    if (stack == 0) {
+                        _.times(slices, (slice) => {
+                            memo.push([
+                                stacks * slices, // top
+                                slice,
+                                (slice + 1) % slices
+                            ]);
+                        });
+                    }
+                    _.times(slices, (slice) => {
+                        var verts = [
+                            stack * slices + slice,
+                            stack * slices + (slice + 1) % slices,
+                            (stack + 1) * slices + slice,
+                            (stack + 1) * slices + (slice + 1) % slices
+                        ];
+                        memo.push([verts[2], verts[1], verts[0]]);
+                        memo.push([verts[1], verts[2], verts[3]]);
+                    });
+                    if (stack == stacks - 2) {
+                        _.times(slices, (slice) => {
+                            memo.push([
+                                stacks * slices + 1, // bottom
+                                (stacks - 1) * slices + (slice + 1) % slices,
+                                (stacks - 1) * slices + slice
+                            ]);
+                            if (memo[memo.length - 1][2] == 73) debugger;
+                        });
+                    }
+                    return memo;
+                }, [])
+                .value(),
+            texture: (function(){
+                var id = new ImageData(256, 256);
+                id.data.fill(0xff);
+                // for (var i=0; i<id.data.length; i+=4) {
+                //     id.data.fill(0x00, i, i+2);
+                // }
+                return id;
+            })()
+        },
+        o: quat.create(),
+        rot: quat.rotateY(quat.create(), quat.create(), Math.PI / 8),
+        pos: vec3.fromValues(0, 3, 10)
+    };
+
     var beacon = {
         model: {
             triangles: [
@@ -81,7 +150,7 @@ $(() => {
         v[2] *= n;
     })
 
-    var entities = [beacon, skybox];
+    var entities = [sphere, beacon, skybox];
 
     var camera = {
         position: vec3.create(),
@@ -165,7 +234,7 @@ $(() => {
             } else {
                 highp float light = dot(vNorm.xyz, vec3(1, 0, 0));
                 light = max(light, 0.0);
-                light += 0.1; // ambient
+                light += 0.01; // ambient
                 gl_FragColor = vec4(color.rgb * light, color.a);
             }
         }
